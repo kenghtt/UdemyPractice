89c5b7aab55ea8bccd4e6f67772f85880a199202
jeremytouch <kenghtt@gmail.com>
2018-05-22 22:30:49 -0500

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Project: JeremyUdemyRepo
+ * Package: PACKAGE_NAME
+ * <p>
+ * User: jeremytouch
+ * Date: 5/22/18
+ * Time: 10:28 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        
+        System.out.println("Hello");
+    }
+}
